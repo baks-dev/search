f338f610746217f8362e5845e5aaff25a2e4d657
MichelAngelo <baksdevelopmen@gmail.com>
2025-10-01 23:32:32 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Resources/assets/search/search.js
+++ Resources/assets/search/search.js
@@ -139,7 +139,7 @@ async function submitSearch()
                 });
 
                 let lazy = document.createElement("script");
-                lazy.src = "/assets/js/lazyload.min.js?v=" + Date.now();
+                lazy.src = "/assets/" + $version + "/js/lazyload.min.js?v=" + Date.now();
                 document.head.appendChild(lazy);
             }
             else
